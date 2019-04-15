c9e733157224cbf78f893fc5de89f575bc396ced
Soulike <316413310@qq.com>
2019-04-15 11:02:02 +0800

    完成框架

---
+++ src/Pages/InsuranceClause/Container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import InsuranceClause from './View';
+
+class InsuranceClauseContainer extends React.Component
+{
+    render()
+    {
+        return <InsuranceClause />;
+    }
+}
+
+export default InsuranceClauseContainer;
---
+++ src/Pages/InsuranceClause/View.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Style from './Style.module.scss';
+
+function InsuranceClause(props)
+{
+    return (
+        <div className={Style.InsuranceClause}>
+
+        </div>
+    );
+}
+
+export default InsuranceClause;
