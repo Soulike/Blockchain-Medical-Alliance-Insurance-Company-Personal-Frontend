f0786b8a5d5c985159f4c103648ec512f6433a29
Soulike <316413310@qq.com>
2019-04-15 10:07:28 +0800

    删除无用路由

--- src/Api/InsurancePurchasingDetail/ROUTE.js
+++ src/Api/InsurancePurchasingDetail/ROUTE.js
@@ -1,11 +1,3 @@
 import {insurancePurchasingDetailPrefix} from './Function';
 
 export const GET_INSURANCE_PURCHASING_INFO = insurancePurchasingDetailPrefix('/getInsurancePurchasingInfo');
-
-export const GET_ELECTRONIC_INSURANCE_POLICY = insurancePurchasingDetailPrefix('/getElectronicInsurancePolicy');
-
-export const GET_MEDICAL_RECORD = insurancePurchasingDetailPrefix('/getMedicalRecord');
-
-export const SUBMIT_INSURANCE_COMPANY_VERIFY_RESULT = insurancePurchasingDetailPrefix('/submitInsuranceCompanyVerifyResult');
-
-export const SUBMIT_PAY_CONFIRMATION = insurancePurchasingDetailPrefix('/submitPayConfirmation');
